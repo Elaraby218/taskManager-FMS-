6c7d67ec5a3234711b76e46c6f5753b1f80ad4f7
Elaraby218 <icpcnews5@gmail.com>
2023-12-29 15:42:58 +0200

    Now its time for Validation Methods for implemented funcitons
    1- class validatino methods added to functinos folder

---
+++ taskManager/functions class/ValidatinoMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskManager.functions_class
+{
+    internal class ValidatinoMethods
+    {
+    }
+}
--- taskManager/obj/Debug/net8.0-windows/taskManager.AssemblyInfo.cs
+++ taskManager/obj/Debug/net8.0-windows/taskManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e295457eec04288090e9e10088600f4a698899d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("taskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
